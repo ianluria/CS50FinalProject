737f03302e902b2ab06bbc1232a7a7fcc1f54fdd
Ian Luria <ian.luria@gmail.com>
2020-02-08 19:00:47 -0700

    Finishing email password support.

--- app/email.py
+++ app/email.py
@@ -2,13 +2,14 @@ from flask_mail import Message
 from app import mail
 from flask import render_template
 from app import app
+from threading import Thread
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 
 def send_password_reset_email(user):
@@ -20,3 +21,7 @@ def send_password_reset_email(user):
                                          user=user, token=token),
                html_body=render_template('email/reset_password.html',
                                          user=user, token=token))
+
+def send_async_email(app, msg):
+    with app.app_context():
+    mail.send(msg)
