98e7167faace646c8f157216d4437a2c46558ed8
Ian Luria <ian.luria@gmail.com>
2020-03-08 19:16:22 -0600

    Adding environmental variables.

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
 login.login_view = 'auth.login'
-login.login_message = _l('Please log in to access this page.')
+login.login_message = ('Please log in to access this page.')
 mail = Mail()
 
 def create_app(config_class=Config):
@@ -25,6 +25,9 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     login.init_app(app)
     mail.init_app(app)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
